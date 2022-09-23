3df8ad28bd5d76b1365b11311ba48e8b558a8ca1
読み上げ <67372333+readloud@users.noreply.github.com>
2022-09-23 10:04:00 +0700

    Add files via upload

---
+++ instascrapper/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Social Media Scrapper',
+    version='1.1.1',
+
+    description='T',
+    long_description = long_description,
+
+    url = 'https://github.com/the-javapocalypse/Social-Media-Scrapper',
+
+    author = 'Muhammad Ali Zia',
+    author_email = 'muhammad.17ali@gmail.com',
+
+     classifiers = [
+
+                       'Development Status :: 4 - Beta',
+
+                       'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3.3',
+                       'Programming Language :: Python :: 3.4',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Programming Language :: Python :: 3.10',
+                       'Programming Language :: Python :: 3.11',
+                   ],
+
+     keywords = 'python twitter tweepy instagram scrapper crawler hashtags hashtag images image caption',
+
+    install_requires = ['tweepy',
+                        'openpyxl',
+                        'selenium'],
+
+
+)
